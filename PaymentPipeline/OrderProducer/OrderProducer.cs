f546c0a9b6a3e9fe2a3eedd6eeb4c0cae15a1235
Guilherme Soster Santos <Guilherme_Santos3@Dell.com>
2019-04-01 17:27:55 -0300

    Added queue feature to OrderConsumer and OrderProducer

--- PaymentPipeline/OrderProducer/OrderProducer.cs
+++ PaymentPipeline/OrderProducer/OrderProducer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace OrderProducer
 {
@@ -6,10 +8,33 @@ namespace OrderProducer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Order Producer Started!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
 
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
 
-            
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "hello",
+                        basicProperties: null,
+                        body: body
+                    );
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
         }
     }
 }
